259c541795d0d312959459dd09238c7ee08ae541
Jebbe9 <jebbede@protonmail.com>
2025-04-11 21:57:33 +0300

    minor changes

--- config.h
+++ config.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const int vertpad            = 15;       /* vertical padding of bar */
-static const int sidepad            = 15;       /* horizontal padding of bar */
+static const int vertpad            = 6;       /* vertical padding of bar */
+static const int sidepad            = 6;       /* horizontal padding of bar */
 
 //static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=14:antialias=true:hinting=true" };
 static const char *fonts[]          = { "Terminus:size=12", };
@@ -38,7 +38,7 @@ static const char col_urgborder[]   = "#ff0000";
 #include "/home/jere/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
-static const char *tags[] = { "br", "te", "mu", "di", "vi", "qb", "ms", };
+static const char *tags[] = { "b", "t", "m", "d", "v", "q", "e", };
 
 static const Rule rules[] = {
 	/* xprop(1):
